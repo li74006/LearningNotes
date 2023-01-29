b35cfd0bf66cf9f0f0a7f8fbdf1b56257f64b015
li74006 <794975037@qq.com>
2023-01-29 18:26:16 +0800

    听完 Nuxt 基础。下一步，仔细研学 Nuxt 文档。

--- Nuxt/playground/pages/index.vue
+++ Nuxt/playground/pages/index.vue
@@ -1,5 +1,23 @@
 <template lang="">
-  <div></div>
+  <div>
+    <h2>hello world</h2>
+    <p>
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Corporis,
+      molestiae, explicabo ipsa debitis saepe maxime deleniti rerum autem esse,
+      recusandae ut adipisci eligendi consequuntur? Accusamus aut voluptatem
+      consequuntur vel? Reprehenderit iste molestiae distinctio natus officia
+      expedita quis architecto debitis soluta, aspernatur voluptatum dolor
+      laudantium exercitationem beatae aliquam dicta nobis at.
+    </p>
+    <p>
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Corporis,
+      molestiae, explicabo ipsa debitis saepe maxime deleniti rerum autem esse,
+      recusandae ut adipisci eligendi consequuntur? Accusamus aut voluptatem
+      consequuntur vel? Reprehenderit iste molestiae distinctio natus officia
+      expedita quis architecto debitis soluta, aspernatur voluptatum dolor
+      laudantium exercitationem beatae aliquam dicta nobis at.
+    </p>
+  </div>
 </template>
 <script>
 export default {};
