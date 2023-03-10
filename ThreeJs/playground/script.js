dbb0ed8c221b84d318c0837c2ddb0b4c2cdfce4d
li74006 <794975037@qq.com>
2023-03-10 22:37:35 +0800

    three.js P28 听到 1h21min 。

--- ThreeJs/playground/script.js
+++ ThreeJs/playground/script.js
@@ -35,17 +35,31 @@ const material = new THREE.ShaderMaterial({
   }
   `,
   fragmentShader: `
+  #define PI 3.1415926535897932384626433832795
+
   varying vec2 vUv;
+
+  float random(vec2 st){
+    return fract(sin(dot(st.xy, vec2(12.9898,78.233))) * 43758.5453123);
+  }
+
+  vec2 rotate(vec2 uv, float rotation, vec2 mid)
+  {
+    return vec2(
+      cos(rotation) * (uv.x - mid.x) + sin(rotation) * (uv.y - mid.y) + mid.x,
+      cos(rotation) * (uv.y - mid.y) - sin(rotation) * (uv.x - mid.x) + mid.y
+    );
+  }
   
   void main(){
     // float strength = vUv.x;
     // float strength = vUv.y;
     // float strength = 1.0 - vUv.x;
     // float strength = vUv.y * 10.0;
-    // float strength = mod(vUv.y * 10.0, 1.0); 
+    // float strength = mod(vUv.y * 10.0, 1.0); // mod 求余函数
 
-    // float strength = mod(vUv.y * 10.0, 1.0); 
-    // strength = step(0.5, strength);
+    // float strength = mod(vUv.y * 10.0, 1.0);
+    // strength = step(0.5, strength); // step 大于 0.5 就是 1，小于就是 0
     // strength = step(0.8, strength);
 
     // float strength = step(0.8, mod(vUv.y * 10.0, 1.0));
@@ -58,12 +72,62 @@ const material = new THREE.ShaderMaterial({
     // barX *= step(0.8, mod(vUv.y * 10.0, 1.0));
     // barY *= step(0.8, mod(vUv.x * 10.0, 1.0));
 
-    float barX = step(0.4, mod(vUv.x * 10.0, 1.0));
-    float barY = step(0.4, mod(vUv.y * 10.0, 1.0));
-    barX *= step(0.8, mod(vUv.y * 10.0 + 0.2, 1.0));
-    barY *= step(0.8, mod(vUv.x * 10.0 + 0.2, 1.0));
+    // float barX = step(0.4, mod(vUv.x * 10.0, 1.0));
+    // float barY = step(0.4, mod(vUv.y * 10.0, 1.0));
+    // barX *= step(0.8, mod(vUv.y * 10.0 + 0.2, 1.0));
+    // barY *= step(0.8, mod(vUv.x * 10.0 + 0.2, 1.0));
+    // float strength = barX + barY;
 
-    float strength = barX + barY;
+    // float strength = abs(vUv.x - 0.5); // abs 绝对值
+
+    // float strength = min(abs(vUv.x - 0.5), abs(vUv.y - 0.5));
+    // float strength = max(abs(vUv.x - 0.5), abs(vUv.y - 0.5));
+
+    // float strength = step(0.2, max(abs(vUv.x - 0.5), abs(vUv.y - 0.5)));
+
+    // float square1 = step(0.2, max(abs(vUv.x - 0.5), abs(vUv.y - 0.5)));
+    // float square2 = 1.0 - step(0.25, max(abs(vUv.x - 0.5), abs(vUv.y - 0.5)));
+    // float strength = square1 * square2;
+
+    // float strength = floor(vUv.x * 10.0) / 10.0; 
+    // strength *= floor(vUv.y * 10.0) / 10.0; 
+
+    // float strength = random(vUv);
+
+    // vec2 gridUv = vec2(floor(vUv.x * 10.0) / 10.0, floor(vUv.y * 10.0) / 10.0);
+    // float strength = random(gridUv);
+    // vec2 gridUv = vec2(floor(vUv.x * 10.0) / 10.0, floor(vUv.x * 5.0 + vUv.y * 10.0) / 10.0);
+    // float strength = random(gridUv);
+
+    // float strength = length(vUv);
+    // float strength = length(vUv - 0.5);
+    // float strength = 1.0 - length(vUv - 0.5);
+    // float strength = distance(vUv, vec2(0.5, 0.5));
+    // float strength = 0.015 / distance(vUv, vec2(0.5));
+    // float strength = 0.015 / distance(vec2(vUv.x * 0.1 + 0.45, vUv.y * 0.5 + 0.25), vec2(0.5));
+
+    // float lightX = 0.015 / distance(vec2(vUv.x * 0.1 + 0.45, vUv.y * 0.5 + 0.25), vec2(0.5));
+    // float lightY = 0.015 / distance(vec2(vUv.y * 0.1 + 0.45, vUv.x * 0.5 + 0.25), vec2(0.5));
+    // float strength = lightX * lightY;
+
+    // vec2 rotatedUv = rotate(vUv, PI * 0.25, vec2(0.5));
+    // float strength = 0.15 / (distance(vec2(rotatedUv.x, (rotatedUv.y - 0.5) * 5.0 + 0.5), vec2(0.5)));
+    // strength *= 0.15 / (distance(vec2(rotatedUv.y, (rotatedUv.x - 0.5) * 5.0 + 0.5), vec2(0.5)));
+
+    // float strength = step(0.25, distance(vUv, vec2(0.5)));
+    // float strength = abs(distance(vUv, vec2(0.5)) - 0.25);
+    // float strength = step(0.01, abs(distance(vUv, vec2(0.5)) - 0.25));
+    // float strength = 1.0 - step(0.01, abs(distance(vUv, vec2(0.5)) - 0.25));
+    // float strength = 1.0 - step(0.01, abs(distance(vec2(vUv.x, vUv.y + sin(vUv.x * 30.0) * 0.1), vec2(0.5)) - 0.25));
+
+
+
+
+
+
+
+
+
 
 
 
