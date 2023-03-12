1b996f579636215c157d739710e60c7ec3025fc5
li74006 <794975037@qq.com>
2023-03-12 23:56:21 +0800

    three.js P30 听完。

--- ThreeJs/playground/script.js
+++ ThreeJs/playground/script.js
@@ -46,6 +46,7 @@ const generateGalaxy = () => {
   const positions = new Float32Array(parameters.count * 3);
   const colors = new Float32Array(parameters.count * 3);
   const scales = new Float32Array(parameters.count * 1);
+  const randomness = new Float32Array(parameters.count * 3);
 
   const insideColor = new THREE.Color(parameters.insideColor);
   const outsideColor = new THREE.Color(parameters.outsideColor);
@@ -75,9 +76,17 @@ const generateGalaxy = () => {
       parameters.randomness *
       radius;
 
-    positions[i3] = Math.cos(branchAngle) * radius + randomX;
-    positions[i3 + 1] = randomY;
-    positions[i3 + 2] = Math.sin(branchAngle) * radius + randomZ;
+    // positions[i3] = Math.cos(branchAngle) * radius + randomX;
+    // positions[i3 + 1] = randomY;
+    // positions[i3 + 2] = Math.sin(branchAngle) * radius + randomZ;
+
+    positions[i3] = Math.cos(branchAngle) * radius;
+    positions[i3 + 1] = 0;
+    positions[i3 + 2] = Math.sin(branchAngle) * radius;
+
+    randomness[i3 + 1] = randomX;
+    randomness[i3 + 2] = randomY;
+    randomness[i3 + 3] = randomZ;
 
     // Color
     const mixedColor = insideColor.clone();
@@ -94,6 +103,10 @@ const generateGalaxy = () => {
   geometry.setAttribute("position", new THREE.BufferAttribute(positions, 3));
   geometry.setAttribute("color", new THREE.BufferAttribute(colors, 3));
   geometry.setAttribute("aScale", new THREE.BufferAttribute(scales, 1));
+  geometry.setAttribute(
+    "aRandomness",
+    new THREE.BufferAttribute(randomness, 3)
+  );
 
   /**
    * Material
@@ -104,29 +117,66 @@ const generateGalaxy = () => {
     vertexColors: true,
 
     vertexShader: `
+    uniform float uTime;
     uniform float uSize;
 
     attribute float aScale;
+    attribute vec3 aRandomness;
+
+    varying vec3 vColor;
 
     void main(){
       vec4 modelPosition = modelMatrix * vec4(position, 1.0);
+      
+      float angle = atan(modelPosition.x, modelPosition.z);
+      float distanceToCenter = length(modelPosition.xz);
+      float angleOffset = (1.0 / distanceToCenter) * uTime * 0.2;
+      angle += angleOffset;
+      modelPosition.x = cos(angle) * distanceToCenter;
+      modelPosition.z = sin(angle) * distanceToCenter;
+
+      modelPosition.xyz += aRandomness;
+
       vec4 viewPosition = viewMatrix * modelPosition;
       vec4 projectedPosition = projectionMatrix * viewPosition;
 
       gl_Position = projectedPosition;
 
+      // Size
       gl_PointSize =  uSize * aScale;
+      gl_PointSize *= (1.0 / - viewPosition.z);
+
+      vColor = color;
     }
     `,
 
     fragmentShader: `
+    varying vec3 vColor;
+
     void main(){
-      gl_FragColor = vec4(1.0, 1.0, 1.0, 1.0);
+      // float strength = distance(gl_PointCoord, vec2(0.5));
+      // strength = step(0.5, strength);
+      // strength = 1.0 -strength;
+
+      // float strength = distance(gl_PointCoord, vec2(0.5));
+      // strength *= 2.0;
+      // strength = 1.0 - strength;
+
+      float strength = distance(gl_PointCoord, vec2(0.5));
+      strength = 1.0 - strength;
+      strength = pow(strength, 10.0);
+
+      vec3 color = mix(vec3(0.0), vColor, strength);
+
+      gl_FragColor = vec4(color, 1.0);
+
+
     }
     `,
 
     uniforms: {
-      uSize: { value: 8 * renderer.getPixelRatio() },
+      uTime: { value: 0 },
+      uSize: { value: 30 * renderer.getPixelRatio() },
     },
   });
 
@@ -229,6 +279,9 @@ const clock = new THREE.Clock();
 
 const tick = () => {
   const elapsedTime = clock.getElapsedTime();
+
+  // Updater material
+  material.uniforms.uTime.value = elapsedTime;
 
   // Update controls
   controls.update();
