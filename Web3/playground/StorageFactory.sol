3c926fbe2805f127fea7edb7f6b769d4f805ae53
li74006 <794975037@qq.com>
2023-03-25 23:58:24 +0800

    solitidy 看到 3:23:14 ~

--- Web3/playground/StorageFactory.sol
+++ Web3/playground/StorageFactory.sol
@@ -4,16 +4,23 @@ pragma solidity ^0.8.0;
 import './SimpleStorage.sol';
 
 contract StorageFactory {
-    SimpleStorage[] public SimpleStorageArray;
+    SimpleStorage[] public simpleStorageArray;
 
     function createSimpleStorageContract() public {
         SimpleStorage simpleStorage = new SimpleStorage();
-        SimpleStorageArray.push(simpleStorage);
+        simpleStorageArray.push(simpleStorage);
     }
 
     function sfStore(uint256 _simpleStorageIndex, uint256 _simpleStorageNumber) public {
         // 当与其他合同互动的时候，需要以下两个参数
         // 1.  Address ： 其他合同的地址
         // 2. ABI ： Application Binary Interface ABI - 告诉合同如何与其他合同互动
+        SimpleStorage simpleStorage = simpleStorageArray[_simpleStorageIndex];
+        simpleStorage.store(_simpleStorageNumber);
+    }
+
+    function sfGet(uint256 _simpleStorageIndex) public view returns(uint256){
+        SimpleStorage simpleStorage = simpleStorageArray[_simpleStorageIndex];
+        return simpleStorage.retrieve(); // retrieve : 找回、取回
     }
 }
