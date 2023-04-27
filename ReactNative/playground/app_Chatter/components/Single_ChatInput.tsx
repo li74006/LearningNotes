c240fd0cdf9522e9f00470a34d8bfda2994ae139
li74006 <794975037@qq.com>
2023-04-27 23:56:30 +0800

    完成 ChatInput 的响应式。

--- ReactNative/playground/app_Chatter/components/Single_ChatInput.tsx
+++ ReactNative/playground/app_Chatter/components/Single_ChatInput.tsx
@@ -1,40 +1,44 @@
+import { useState } from "react";
 import { SafeAreaView, View, Text, TextInput, InputAccessoryView, StyleSheet } from "react-native";
 
 export default function Single_ChatInput() {
-  const styles = StyleSheet.create({
-    container: {
-      backgroundColor: "lightgray",
-      borderRadius: 100,
-      flexDirection: "row",
-      alignSelf: "flex-end",
-    },
+  const [input, setInput] = useState("");
 
-    input: {
-      flex: 1,
-      paddingLeft: 20,
-    },
-
-    plus: {
-      width: 24,
-      height: 24,
-      alignItems: "center",
-      justifyContent: "center",
-      backgroundColor: "dodgerblue",
-      marginVertical: 6,
-      marginRight: 8,
-      borderRadius: 20,
-    },
-
-    text_plus: {
-      color: "white",
-    },
-  });
   return (
     <View style={styles.container}>
-      <TextInput style={styles.input} />
+      <TextInput value={input} onChangeText={setInput} style={styles.input} placeholder="Shoot something..." />
       <View style={styles.plus}>
         <Text style={styles.text_plus}>+</Text>
       </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "lightgray",
+    borderRadius: 100,
+    flexDirection: "row",
+    alignSelf: "flex-end",
+  },
+
+  input: {
+    flex: 1,
+    paddingLeft: 20,
+  },
+
+  plus: {
+    width: 24,
+    height: 24,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "dodgerblue",
+    marginVertical: 6,
+    marginRight: 8,
+    borderRadius: 20,
+  },
+
+  text_plus: {
+    color: "white",
+  },
+});
