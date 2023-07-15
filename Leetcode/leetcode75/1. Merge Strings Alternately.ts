44603621601ccbb1eac05cc9bc8208a87bc069ed
li74006 <794975037@qq.com>
2023-07-15 10:48:37 +0800

    optimized leetcode75-1

--- Leetcode/leetcode75/1. Merge Strings Alternately.ts
+++ Leetcode/leetcode75/1. Merge Strings Alternately.ts
@@ -2,10 +2,10 @@ function mergeAlternately(word1: string, word2: string): string {
   let afterMerge: string = "";
 
   for (let i = 0; i < word1.length || i < word2.length; i++) {
-    let frontWord = word1[i] ? word1[i] : "";
-    let backWord = word2[i] ? word2[i] : "";
-    let merge = frontWord + backWord;
-    afterMerge += merge;
+    let frontWord = word1[i] ?? "";
+    let backWord = word2[i] ?? "";
+
+    afterMerge += frontWord + backWord;
   }
 
   return afterMerge;
